5a8b0c500b24aafe6b4df864a3c83eafae35c616
Casey Vaughn <caseyvaughn@caseys-mbp-2.myfiosgateway.com>
2021-12-13 11:55:41 -0500

    added search form

--- assets/script.js
+++ assets/script.js
@@ -1,50 +1,84 @@
 
 //API RESOURCES
-// all cards API = "https://rws-cards-api.herokuapp.com/api/v1/cards"
-// random card API = `https://rws-cards-api.herokuapp.com/api/v1/cards/random?n=${number}`
-// card search API = `https://rws-cards-api.herokuapp.com/api/v1/cards/search?q=${cardName}`
+// const allCardsAPI = "https://rws-cards-api.herokuapp.com/api/v1/cards"
+const randomAPI = "https://rws-cards-api.herokuapp.com/api/v1/cards/random?n=1"
+// const cardSearchAPI = `https://rws-cards-api.herokuapp.com/api/v1/cards/search?q=${cardName}`
 
 //querySelectors
 const cardDiv = document.querySelector("#card-data");
 const drawOneCard = document.querySelector("#random-card")
+const cardSearch = document.querySelector("#card-search")
+const searchForm = document.querySelector("#card-form")
+
 
 
 //generate 1 random card
 async function fetchRandomCard() {
   try {
-    const url = "https://rws-cards-api.herokuapp.com/api/v1/cards/random?n=1"
-    const res = await axios.get(url);
+    // const url = "https://rws-cards-api.herokuapp.com/api/v1/cards/random?n=1"
+    // const res = await axios.get(url);
+    const res = await axios.get(randomAPI);
     const cardData = res.data;
     console.log(cardData);
+    //clear previously displayed card
+    cardDiv.innerHTML = "";
     showCardData(cardData.cards[0]);
   } catch (error) {
     console.log("ERROR!!!!")
   }
 }
 
+//search for a card
+async function fetchCardData(cardName) {
+  try {
+    const cardSearchAPI = `https://rws-cards-api.herokuapp.com/api/v1/cards/search?q=${cardName}`
+    const res = await axios.get(cardSearchAPI);
+    const cardData = res.data;
+    console.log(cardData);
+    showCardData(cardData.cards[0]);
+  } catch (error) {
+    console.log("ERROR!!!")
+  }
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  //clear previously displayed cards
+  cardDiv.innerHTML = "";
+  let inputValue = cardSearch.value;
+  //clear search input
+  cardSearch.value = "";
+  fetchCardData(inputValue);
+}
 
 function showCardData(cardObj) {
   //card name
   const cardName = document.createElement("h2");
   cardName.innerText = cardObj.name;
   cardDiv.appendChild(cardName);
-  console.log(cardName);
+  //console.log(cardName);
+
   //card type
   const cardType = document.createElement("h3");
   cardType.innerText = cardObj.type;
   cardDiv.appendChild(cardType);
-  console.log(cardType);
+  //console.log(cardType);
+
   //card meaning
   const cardMeaningUp = document.createElement("h3");
   cardMeaningUp.innerText = cardObj.meaning_up;
   cardDiv.appendChild(cardMeaningUp);
-  console.log(cardMeaningUp);
+  //console.log(cardMeaningUp);
+
   //card description
   const cardDesc = document.createElement("h4");
   cardDesc.innerText = cardObj.desc;
   cardDiv.appendChild(cardDesc);
-  console.log(cardDesc);
+  //console.log(cardDesc);
 }
 
 //add event listener for random card buttom
 drawOneCard.addEventListener("click", fetchRandomCard);
+//add event listener for card search 
+searchForm.addEventListener("submit", handleSubmit);
+
