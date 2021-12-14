9614275291fa5a1d8d56489104084ee762fb320e
Casey Vaughn <caseyvaughn@caseys-mbp-2.myfiosgateway.com>
2021-12-14 16:55:54 -0500

    save for day 2

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,3 @@
-
 //API RESOURCES
 // const allCardsAPI = "https://rws-cards-api.herokuapp.com/api/v1/cards"
 const randomAPI = "https://rws-cards-api.herokuapp.com/api/v1/cards/random?n=1"
@@ -12,14 +11,13 @@ const cardSearch = document.querySelector("#card-search")
 const searchForm = document.querySelector("#card-form")
 const imageDiv = document.querySelector("#card-image");
 // const image = document.querySelector("img");
-
+const randomNum = Math.random();
+console.log(randomNum);
 
 
 //generate 1 random card
 async function fetchRandomCard() {
   try {
-    // const url = "https://rws-cards-api.herokuapp.com/api/v1/cards/random?n=1"
-    // const res = await axios.get(url);
     const res = await axios.get(randomAPI);
     const cardData = res.data;
     console.log(cardData);
@@ -39,7 +37,6 @@ async function fetchThreeCards() {
       const res = await axios.get(randomAPI);
       const cardData = res.data;
       console.log(cardData);
-      // cardDiv.innerHTML = "";
       showCardData(cardData.cards[0]);
       displayImages(cardData.cards[0])
     }
@@ -63,9 +60,7 @@ async function fetchCardData(cardName) {
 }
 
 
-
 function displayImages(cardObj) {
-  // imageDiv.innerHTML = "";
   const img = document.createElement("img");
   const cardShort = cardObj.name_short;
   img.src = `https://www.sacred-texts.com/tarot/pkt/img/${cardShort}.jpg`
@@ -96,7 +91,7 @@ function showCardData(cardObj) {
   //console.log(cardType);
 
   //card direction probability logic
-  if (Math.random() < 0.5) {
+  if (randomNum < .5) {
     //card meaning up
     const cardMeaningUp = document.createElement("h3");
     cardMeaningUp.innerText = `Meaning (Up): ${cardObj.meaning_up}`;
@@ -135,3 +130,43 @@ drawThreeCards.addEventListener("click", fetchThreeCards);
 //add event listener for card search 
 searchForm.addEventListener("submit", handleSubmit);
 
+
+//old showcarddata function
+// function showCardData(cardObj) {
+//   //card name
+//   const cardName = document.createElement("h2");
+//   cardName.innerText = cardObj.name;
+//   cardDiv.appendChild(cardName);
+//   //console.log(cardName);
+
+//   //card type
+//   const cardType = document.createElement("h3");
+//   cardType.innerText = `Type: ${cardObj.type}`;
+//   cardDiv.appendChild(cardType);
+//   //console.log(cardType);
+
+//   //card direction probability logic
+//   if (Math.random() < 0.5) {
+//     //card meaning up
+//     const cardMeaningUp = document.createElement("h3");
+//     cardMeaningUp.innerText = `Meaning (Up): ${cardObj.meaning_up}`;
+//     cardDiv.appendChild(cardMeaningUp);
+//     console.log(cardMeaningUp);
+
+//     // image.classList = "img-norm";
+//     imageDiv.classList = "img-norm";
+//   } else {
+//     //card meaning rev 
+//     const cardMeaningRev = document.createElement("h3");
+//     cardMeaningRev.innerText = `Meaning (Reverse): ${cardObj.meaning_rev}`
+//     cardDiv.appendChild(cardMeaningRev);
+//     // console.log(cardMeaningRev);
+//     // image.classList = "img-rev";
+//     imageDiv.classList = "img-rev";
+//   }
+//   //card description
+//   const cardDesc = document.createElement("h4");
+//   cardDesc.innerText = cardObj.desc;
+//   cardDiv.appendChild(cardDesc);
+//   //console.log(cardDesc);
+// }
