c5fbd94fc15262fc49c17f85498c64776523f4ac
Casey Vaughn <caseyvaughn@caseys-mbp-2.myfiosgateway.com>
2021-12-15 10:11:10 -0500

    fixed search function

--- assets/script.js
+++ assets/script.js
@@ -45,36 +45,11 @@ async function fetchThreeCards() {
   }
 }
 
-//search for a card
-async function fetchCardData(cardName) {
-  try {
-    const cardSearchAPI = `https://rws-cards-api.herokuapp.com/api/v1/cards/search?q=${cardName}`
-    const res = await axios.get(cardSearchAPI);
-    const cardData = res.data;
-    console.log(cardData);
-    showCardData(cardData.cards[0]);
-    displayImages(cardData.cards[0])
-  } catch (error) {
-    console.log("FETCH CARD DATA ERROR!!!")
-  }
-}
-
-
 function displayImages(cardObj) {
   const img = document.createElement("img");
   const cardShort = cardObj.name_short;
   img.src = `https://www.sacred-texts.com/tarot/pkt/img/${cardShort}.jpg`
   imageDiv.appendChild(img);
-}
-
-function handleSubmit(event) {
-  event.preventDefault();
-  //clear previously displayed cards
-  cardDiv.innerHTML = "";
-  let inputValue = cardSearch.value;
-  //clear search input
-  cardSearch.value = "";
-  fetchCardData(inputValue);
 }
 
 function showCardData(cardObj) {
@@ -91,40 +66,83 @@ function showCardData(cardObj) {
   //console.log(cardType);
 
   //card direction probability logic
-  if (randomNum < .5) {
+  if (Math.random() < .5) {
     //card meaning up
     const cardMeaningUp = document.createElement("h3");
     cardMeaningUp.innerText = `Meaning (Up): ${cardObj.meaning_up}`;
     cardDiv.appendChild(cardMeaningUp);
     console.log(cardMeaningUp);
-
-    // image.classList = "img-norm";
     imageDiv.classList = "img-norm";
   } else {
     //card meaning rev 
     const cardMeaningRev = document.createElement("h3");
     cardMeaningRev.innerText = `Meaning (Reverse): ${cardObj.meaning_rev}`
     cardDiv.appendChild(cardMeaningRev);
-    // console.log(cardMeaningRev);
-    // image.classList = "img-rev";
     imageDiv.classList = "img-rev";
   }
   //card description
   const cardDesc = document.createElement("h4");
   cardDesc.innerText = cardObj.desc;
   cardDiv.appendChild(cardDesc);
-  //console.log(cardDesc);
 }
 
 
-function showClick() {
-  console.log("CLICK!");
+function showSearchedCardData(cardObj) {
+  //card name
+  const cardName = document.createElement("h2");
+  cardName.innerText = cardObj.name;
+  cardDiv.appendChild(cardName);
+
+  //card type
+  const cardType = document.createElement("h3");
+  cardType.innerText = `Type: ${cardObj.type}`;
+  cardDiv.appendChild(cardType);
+
+  //card meaning up 
+  const cardMeaningUp = document.createElement("h3");
+  cardMeaningUp.innerText = `Meaning (Up): ${cardObj.meaning_up}`;
+  cardDiv.appendChild(cardMeaningUp);
+
+  //card meaning rev 
+  const cardMeaningRev = document.createElement("h3");
+  cardMeaningRev.innerText = `Meaning (Reverse): ${cardObj.meaning_rev}`
+  cardDiv.appendChild(cardMeaningRev);
+
+  //card description
+  const cardDesc = document.createElement("h4");
+  cardDesc.innerText = cardObj.desc;
+  cardDiv.appendChild(cardDesc);
 }
 
-// return Math.random() < 0.5;
+//fetch dara for card search function 
+async function fetchCardData(cardName) {
+  try {
+    const cardSearchAPI = `https://rws-cards-api.herokuapp.com/api/v1/cards/search?q=${cardName}`
+    const res = await axios.get(cardSearchAPI);
+    const cardData = res.data;
+    cardDiv.innerHTML = "";
+    imageDiv.innerHTML = "";
+    showSearchedCardData(cardData.cards[0]);
+    displayImages(cardData.cards[0])
+  } catch (error) {
+    console.log("FETCH CARD DATA ERROR!!!")
+  }
+}
+
+//card search function
+function handleSubmit(event) {
+  event.preventDefault();
+  //clear previously displayed cards
+  cardDiv.innerHTML = "";
+  let inputValue = cardSearch.value;
+  //clear search input
+  cardSearch.value = "";
+  fetchCardData(inputValue);
+}
+
+//EVENT LISTENERS//
 //add event listener for random card buttom
 drawOneCard.addEventListener("click", fetchRandomCard);
-drawOneCard.addEventListener("click", showClick);
 //add event listener for three card option
 drawThreeCards.addEventListener("click", fetchThreeCards);
 //add event listener for card search 
